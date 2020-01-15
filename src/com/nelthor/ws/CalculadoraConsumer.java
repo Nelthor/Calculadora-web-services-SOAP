3be7b05e14b76d4d49906a8cbce9cc4fd68a6f90
NelsonPosada <posada.nj@gmail.com>
2020-01-14 20:33:42 -0500

    Agregamos CalculadoraConsumer para consumir el servicio ya creado

---
+++ src/com/nelthor/ws/CalculadoraConsumer.java
@@ -0,0 +1,26 @@
+package com.nelthor.ws;
+
+public class CalculadoraConsumer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CalculadoraImplService calculadoraService= new CalculadoraImplService();
+		
+		Calculadora consumer=calculadoraService.getCalculadoraImplPort();
+		
+		System.out.println("Suma");
+		System.out.println(consumer.operacion(1, 6, 4));
+		
+		System.out.println("Resta");
+		System.out.println(consumer.operacion(2, 6, 4));
+		
+		System.out.println("Multiplicacion");
+		System.out.println(consumer.operacion(3, 6, 4));
+		
+		System.out.println("Division");
+		System.out.println(consumer.operacion(4, 6, 4));
+		
+	}
+
+}
