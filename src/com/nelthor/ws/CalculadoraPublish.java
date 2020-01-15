74270a6c2fbdd51afbf735ce7fdf72bf08c0361f
NelsonPosada <posada.nj@gmail.com>
2020-01-14 20:03:24 -0500

    Se publico el servicio en la CalculadoraPublish

--- src/com/nelthor/ws/CalculadoraPublish.java
+++ src/com/nelthor/ws/CalculadoraPublish.java
@@ -1,10 +1,12 @@
 package com.nelthor.ws;
 
+import javax.xml.ws.Endpoint;
+
 public class CalculadoraPublish {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Endpoint.publish("http://localhost:1515/WS/Calculadora", new CalculadoraImpl());
 	}
 
 }
